0e7b8481577bdd40169b2d1b92484e5666df9e77
YoungCow_code <youngcowcode@gmail.com>
2022-02-05 00:27:31 -0700

    Search 구현 및 OMDB 연결(axios)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    
+    <Logo />
     <div class="nav nav-pills">
       <div class="nav-item" v-for="nav in navigations" :key="nav.name">
         <RouterLink :to="nav.href" active-class="active" class="nav-link">
@@ -12,7 +12,12 @@
 </template>
 
 <script>
+import Logo from './Logo.vue'
+
 export default {
+  components:{
+    Logo
+  },
   data(){
     return{
       navigations: [
@@ -33,3 +38,15 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  header{
+    height: 70px;
+    padding: 0 40px;
+    display: flex;
+    align-items: center;
+    .logo{
+      margin-right: 40px;
+    }
+  }
+</style>
